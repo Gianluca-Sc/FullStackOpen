5cdb448fdba7427e7861208e370f1685843f01a9
Gianluca-Sc <scibettagianluca@gmail.com>
2023-08-01 23:31:50 +0200

    add route testing for part 5

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -25,6 +25,12 @@ app.use(middleware.tokenExtractor);
 app.use("/api/blogs", BlogRouter);
 app.use("/api/users", UserRouter);
 app.use("/api/login", loginRouter);
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
